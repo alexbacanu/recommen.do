8c0f80f69729dd1e625be687d02927cb0b537f23
Alexandru Bacanu <alex.bacanu@live.com>
2023-06-07 15:59:34 +0300

    add plasmo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <main className="flex min-h-screen flex-col items-center justify-between p-24">Hello</main>;
+  return <main className="placeholder">Hello</main>;
 }
---
+++ src/popup/index.tsx
@@ -0,0 +1,9 @@
+import "~/styles/globals.css";
+
+export default function IndexPopup() {
+  return (
+    <section id="popup" className="placeholder">
+      Hello
+    </section>
+  );
+}
