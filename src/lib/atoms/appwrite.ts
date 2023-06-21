664d563d0431b5d62b3ec3df6e3ba05e707fbc6c
Alexandru Bacanu <alex.bacanu@live.com>
2023-06-21 19:32:47 +0300

    change type of profile atom

--- src/lib/atoms/appwrite.ts
+++ src/lib/atoms/appwrite.ts
@@ -1,9 +1,10 @@
+import type { Profile } from "~/lib/types";
 import type { Models } from "appwrite/types/models";
 
 import { atom } from "jotai";
 
 export const accountAtom = atom<null | Models.User<Models.Preferences>>(null);
-export const profileAtom = atom<null | Models.Document>(null);
+export const profileAtom = atom<null | Profile>(null);
 
 export const isLoadingAtom = atom<boolean>(true);
 
