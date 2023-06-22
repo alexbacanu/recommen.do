6fcbbb699419f0d95bc7909299daf074fcd86f35
Alexandru Bacanu <alex.bacanu@live.com>
2023-06-22 20:14:52 +0300

    improved buttons

--- src/components/tabs/subscription.tsx
+++ src/components/tabs/subscription.tsx
@@ -6,7 +6,7 @@ import type { Profile } from "~/lib/types";
 import { useStorage } from "@plasmohq/storage/hook";
 import { useQuery } from "@tanstack/react-query";
 
-import { buttonVariants } from "~/components/ui/button";
+import { Button } from "~/components/ui/button";
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "~/components/ui/card";
 import { appwriteUrl } from "~/lib/envClient";
 import { AppwriteService } from "~/lib/helpers/appwrite-service";
@@ -112,30 +112,24 @@ export function Subscription({ profile }: SubscriptionProps) {
         </CardContent>
         {hasSubscription ? (
           <CardFooter className="grid grid-cols-2 gap-4">
-            <a
-              href={refillURL}
-              target={target}
-              className={buttonVariants({ variant: "outline", size: "lg", className: "text-center" })}
-            >
-              {refillQuery.isLoading ? "Loading..." : "Add 50 more recommendations"}
-            </a>
-            <a
-              href={subURL}
-              target={target}
-              className={buttonVariants({ variant: "outline", size: "lg", className: "text-center" })}
-            >
-              {subQuery.isLoading ? "Loading..." : "Manage subscription"}
-            </a>
+            <Button variant="outline" className="text-center" size="lg" disabled={refillQuery.isLoading}>
+              <a href={refillURL} target={target}>
+                Add 50 more recommendations
+              </a>
+            </Button>
+            <Button variant="outline" className="text-center" size="lg" disabled={subQuery.isLoading}>
+              <a href={subURL} target={target}>
+                Manage subscription
+              </a>
+            </Button>
           </CardFooter>
         ) : (
           <CardFooter className="grid grid-cols-1">
-            <a
-              href={`${appwriteUrl}/#pricing`}
-              target={target}
-              className={buttonVariants({ variant: "outline", size: "lg" })}
-            >
-              View subscription options
-            </a>
+            <Button variant="outline" size="lg">
+              <a href={`${appwriteUrl}/#pricing`} target={target}>
+                View subscription options
+              </a>
+            </Button>
           </CardFooter>
         )}
       </Card>
