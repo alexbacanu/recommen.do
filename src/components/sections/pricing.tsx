ae050b7ebeb93769204c975e4b7cf65fca3f4d5f
Alexandru Bacanu <alex.bacanu@live.com>
2023-06-22 18:04:23 +0300

    use tanstack query for subscriptions

--- src/components/sections/pricing.tsx
+++ src/components/sections/pricing.tsx
@@ -3,25 +3,44 @@
 import type { PricingPlan } from "~/lib/types";
 import type { Variants } from "framer-motion";
 
+import { useQuery } from "@tanstack/react-query";
 import { motion } from "framer-motion";
 import { useAtomValue } from "jotai";
 import Link from "next/link";
-import { useState } from "react";
 
 import { Badge } from "~/components/ui/badge";
-import { Button, buttonVariants } from "~/components/ui/button";
+import { buttonVariants } from "~/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "~/components/ui/card";
 import { Label } from "~/components/ui/label";
 import { accountAtom, profileAtom } from "~/lib/atoms/appwrite";
 import { appwriteUrl, authuiSite } from "~/lib/envClient";
-import { useAppwrite } from "~/lib/helpers/use-appwrite";
+import { AppwriteService } from "~/lib/helpers/appwrite-service";
 
 interface PricingProps {
   plans: PricingPlan[];
 }
 
+let jwt: string;
+async function getCheckoutURL(priceId: string) {
+  if (!jwt) {
+    const jwtObject = await AppwriteService.createJWT();
+    jwt = jwtObject.jwt;
+  }
+
+  const response = await fetch(`${appwriteUrl}/api/stripe/subscription/${priceId}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${jwt}`,
+    },
+  });
+
+  const checkoutURL = await response.json();
+
+  return checkoutURL;
+}
+
 export function Pricing({ plans }: PricingProps) {
-  const [loading, setLoading] = useState(false);
+  const profile = useAtomValue(profileAtom);
 
   const container: Variants = {
     hidden: { opacity: 0 },
@@ -36,46 +55,6 @@ export function Pricing({ plans }: PricingProps) {
     },
   };
 
-  const zoom: Variants = {
-    hidden: { scale: 0.95 },
-    visible: {
-      scale: 1,
-      transition: {
-        ease: "easeOut",
-      },
-    },
-  };
-
-  const { createJWT } = useAppwrite();
-  const account = useAtomValue(accountAtom);
-  const profile = useAtomValue(profileAtom);
-
-  const showSubscribe = profile && !profile.stripeSubscriptionId;
-  const showManage = profile && profile.stripeSubscriptionId;
-
-  let jwt: string;
-
-  const handleSubscribe = async (priceId: string) => {
-    setLoading(true);
-
-    if (!jwt) {
-      const jwtToken = await createJWT();
-      jwt = jwtToken.jwt;
-    }
-
-    const getCheckoutURL = await fetch(`${appwriteUrl}/api/stripe/subscription/${priceId}`, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${jwt}`,
-      },
-    });
-
-    const checkoutUrl = await getCheckoutURL.json();
-
-    window.open(checkoutUrl.url, "_blank", "noopener,noreferrer");
-    setLoading(false);
-  };
-
   return (
     <section id="pricing" className="overflow-hidden">
       <div className="mx-auto max-w-7xl px-4 py-8 lg:py-16">
@@ -88,13 +67,21 @@ export function Pricing({ plans }: PricingProps) {
               <Label className="text-center text-sm md:text-base">10 free recommendations on account creation</Label>
             </div>
             <div className="flex items-center gap-x-2 md:gap-x-6">
-              <Link
-                className={buttonVariants({ variant: "outline", className: "whitespace-nowrap" })}
-                href={authuiSite}
-              >
-                Get started
-              </Link>
-              <Label className="text-base">0$ / month</Label>
+              {profile ? (
+                <>
+                  <Label className="text-base">Already claimed</Label>
+                </>
+              ) : (
+                <>
+                  <Link
+                    className={buttonVariants({ variant: "outline", className: "whitespace-nowrap" })}
+                    href={authuiSite}
+                  >
+                    Get started
+                  </Link>
+                  <Label className="text-base">0$ / month</Label>
+                </>
+              )}
             </div>
           </Card>
 
@@ -106,54 +93,91 @@ export function Pricing({ plans }: PricingProps) {
             className="grid grid-cols-1 gap-x-4 gap-y-6 lg:grid-cols-3 lg:gap-x-8"
           >
             {plans.map((plan, index) => (
-              <motion.div key={plan.priceId} variants={zoom} viewport={{ once: true }}>
-                <Card className={index === 1 ? "border-ring" : ""}>
-                  <CardHeader>
-                    <CardTitle className="flex items-start justify-between">
-                      {plan.name}
-                      <Badge variant="outline">Beta</Badge>
-                    </CardTitle>
-                    <CardDescription>{plan.price}$ / month</CardDescription>
-                  </CardHeader>
-                  <CardContent className="grid">
-                    {!account ? (
-                      <Link
-                        className={buttonVariants({ variant: index === 1 ? "default" : "outline" })}
-                        href={authuiSite}
-                      >
-                        Get started
-                      </Link>
-                    ) : (
-                      <Button
-                        variant={index === 1 ? "default" : "outline"}
-                        onClick={() => handleSubscribe(plan.priceId)}
-                        disabled={loading}
-                      >
-                        {showSubscribe && "Subscribe"}
-                        {showManage && "Manage subscription"}
-                      </Button>
-                    )}
-                  </CardContent>
-                  <CardFooter>
-                    <div className="flex flex-col gap-y-4">
-                      {plan.metadata.features &&
-                        plan.metadata.features.split(",").map((feature, index) => (
-                          <div
-                            key={index}
-                            className="flex items-center gap-x-2 text-muted-foreground first:text-card-foreground"
-                          >
-                            <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
-                            <p className="text-sm font-medium leading-none">{feature}</p>
-                          </div>
-                        ))}
-                    </div>
-                  </CardFooter>
-                </Card>
-              </motion.div>
+              <PricingCard key={plan.priceId} plan={plan} index={index} />
             ))}
           </motion.div>
         </div>
       </div>
     </section>
+  );
+}
+
+function PricingCard({ plan, index }: { plan: PricingPlan; index: number }) {
+  const account = useAtomValue(accountAtom);
+  const profile = useAtomValue(profileAtom);
+
+  const showGetStarted = !account;
+  const showSubscribe = profile && !profile.stripeSubscriptionId;
+  const showManageSubscription = profile && profile.stripeSubscriptionId;
+
+  const needsVerification = account && !profile;
+
+  const { data, isLoading } = useQuery<{ url: string }>({
+    queryKey: ["getCheckoutURL", plan.priceId],
+    queryFn: () => getCheckoutURL(plan.priceId),
+    refetchOnMount: false,
+    refetchOnReconnect: false,
+    refetchOnWindowFocus: false,
+    cacheTime: 1000 * 60 * 60 * 24, // 24 hours
+    retry: false,
+  });
+
+  const checkoutURL = data ? data.url : "";
+
+  const zoom: Variants = {
+    hidden: { scale: 0.95 },
+    visible: {
+      scale: 1,
+      transition: {
+        ease: "easeOut",
+      },
+    },
+  };
+
+  return (
+    <motion.div key={plan.priceId} variants={zoom} viewport={{ once: true }}>
+      <Card className={index === 1 ? "border-ring" : ""}>
+        <CardHeader>
+          <CardTitle className="flex items-start justify-between">
+            {plan.name}
+            <Badge variant="outline">Beta</Badge>
+          </CardTitle>
+          <CardDescription>{plan.price}$ / month</CardDescription>
+        </CardHeader>
+        <CardContent className="grid">
+          {showGetStarted && (
+            <Link href={authuiSite} className={buttonVariants({ variant: index === 1 ? "default" : "outline" })}>
+              Get started
+            </Link>
+          )}
+
+          {showSubscribe && (
+            <Link
+              href={needsVerification ? "/profile" : checkoutURL}
+              className={buttonVariants({ variant: index === 1 ? "default" : "outline" })}
+            >
+              {isLoading ? "Loading..." : "Subscribe now"}
+            </Link>
+          )}
+
+          {showManageSubscription && (
+            <Link href={checkoutURL} className={buttonVariants({ variant: index === 1 ? "default" : "outline" })}>
+              {isLoading ? "Loading..." : "Manage subscription"}
+            </Link>
+          )}
+        </CardContent>
+        <CardFooter>
+          <div className="flex flex-col gap-y-4">
+            {plan.metadata.features &&
+              plan.metadata.features.split(",").map((feature, index) => (
+                <div key={index} className="flex items-center gap-x-2 text-muted-foreground first:text-card-foreground">
+                  <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
+                  <p className="text-sm font-medium leading-none">{feature}</p>
+                </div>
+              ))}
+          </div>
+        </CardFooter>
+      </Card>
+    </motion.div>
   );
 }
---
+++ src/lib/helpers/appwrite-service.ts
@@ -0,0 +1,60 @@
+import type { Profile } from "~/lib/types";
+
+import { Account, Client, Databases, Query } from "appwrite";
+
+import { appwriteEndpoint, appwriteProject } from "~/lib/envClient";
+
+const client = new Client();
+client.setEndpoint(appwriteEndpoint).setProject(appwriteProject);
+
+const account = new Account(client);
+const databases = new Databases(client);
+
+export const AppwriteService = {
+  getAccount: async () => {
+    const user = await account.get();
+    console.log("getAccount:", user);
+
+    if (user) {
+      return user;
+    }
+    return null;
+  },
+
+  getProfile: async () => {
+    const { documents } = await databases.listDocuments<Profile>("main", "profile", [Query.limit(1)]);
+    console.log("getProfile:", documents[0]);
+
+    if (documents.length > 0) {
+      return documents[0];
+    }
+    return null;
+  },
+
+  createJWT: async () => {
+    const jwt = await account.createJWT();
+    console.log("createJWT:", jwt);
+
+    return jwt;
+  },
+
+  setOAuth2: async (provider: string) => {
+    const user = account.createOAuth2Session(provider, "http://localhost:1947/api/appwrite/auth");
+    console.log("setOAuth2:", user);
+
+    if (user) {
+      return user;
+    }
+    return null;
+  },
+
+  setSession: async (hash: string) => {
+    const authCookies: Record<string, string> = {};
+    authCookies["a_session_" + appwriteProject] = hash;
+    client.headers["X-Fallback-Cookies"] = JSON.stringify(authCookies);
+  },
+
+  signOut: async () => {
+    await account.deleteSession("current");
+  },
+};
