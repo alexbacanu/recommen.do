f1aa382d15c1ffdb71d872dcbeeb20776f3d46c9
Alexandru Bacanu <alex.bacanu@live.com>
2023-07-04 00:46:32 +0300

    Fix hover on badges

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -10,10 +10,10 @@ const badgeVariants = cva(
   {
     variants: {
       variant: {
-        default: "border-transparent bg-primary text-primary-foreground shadow hover:bg-primary/80",
-        secondary: "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        destructive: "border-transparent bg-destructive text-destructive-foreground shadow hover:bg-destructive/80",
-        outline: "bg-popover text-secondary-foreground hover:bg-secondary/80",
+        default: "border-transparent bg-primary text-primary-foreground shadow",
+        secondary: "border-transparent bg-secondary text-secondary-foreground",
+        destructive: "border-transparent bg-destructive text-destructive-foreground shadow",
+        outline: "bg-popover text-secondary-foreground",
       },
     },
     defaultVariants: {
